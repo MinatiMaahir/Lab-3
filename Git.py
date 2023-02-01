333f11f6b408fe475ae4d61a847df4950a41e97e
MinatiMaahir <maahirchugh1724@gmail.com>
2023-02-01 16:57:12 +0530

    hello

---
+++ Git.py
@@ -0,0 +1,18 @@
+class shape:
+    pass
+       
+class circle(shape):
+    def m(self):
+        print("The area of circle is: 100")
+        print("The color of circle is: Blue")
+ 
+class rectangle(shape):
+    def m(self):
+        print("The area of rectangle is: 100")
+        print("The color of rectangle is: Red")  
+     
+obj1 = rectangle()
+obj1.m()
+
+obj2 = circle()
+obj2.m()
